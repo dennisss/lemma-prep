dddc44b8fe6e64599f277488d5f19b083ecaf1e6
Dennis Shtatnov <densht@gmail.com>
2018-03-14 10:38:11 -0400

    Renaming files, changing urls, adding footer

---
+++ README.md
@@ -0,0 +1,15 @@
+Lemma Prep Page
+===============
+
+Requirements
+------------
+
+- Node.js on computer
+
+
+Instructions
+----------
+- Run `npm install` in the directory once
+- Each time during during development
+	- Run `./start.sh` to start up the development server
+	- Via site at `http://localhost:8001/prep`
---
+++ start.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var express = require('express'),
+	path = require('path');
+
+var app = express();
+
+app.use('/prep', express.static(__dirname + '/public'));
+
+app.use('/*', (req, res) => {
+	res.redirect('https://www.lem.ma' + req.originalUrl)
+})
+
+app.listen(8001, function() {
+	console.log('Running simple Lemma server listening on port 8001!')
+});
